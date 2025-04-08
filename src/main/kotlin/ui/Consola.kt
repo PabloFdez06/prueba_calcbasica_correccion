fe6798231d19f538ff456eb2b41374759b41ed88
PabloFdez06 <ffpablo@iesrafaelalberti.es.>
2025-04-08 16:10:13 +0200

    Para parte 2, ficheros de mi práctica de calculadora subidos, empiezo con mi proyecto como base

--- src/main/kotlin/ui/Consola.kt
+++ src/main/kotlin/ui/Consola.kt
@@ -1,52 +1,27 @@
-package es.iesraprog2425.pruebaes.ui
+package org.example.ui
 
-import java.util.Scanner
+import org.example.model.Calculadora
+import org.example.model.Operadores
+import java.util.*
 
-class Consola : IEntradaSalida {
-    private val scanner = Scanner(System.`in`)
+class Consola: IConsola {
 
-    override fun mostrar(msj: String, salto: Boolean) {
-        print("$msj${if (salto) "\n" else ""}")
+    val scanner = Scanner(System.`in`)
+
+
+    override fun mostrar(msj: String) {
+        println(msj)
     }
 
-    override fun mostrarError(msj: String, salto: Boolean) {
-        mostrar("ERROR - $msj", salto)
+    override fun mostrarError(msj: String) {
+        println(msj)
     }
 
     override fun pedirInfo(msj: String): String {
-        if (msj.isNotEmpty()) mostrar(msj, false)
-        return scanner.nextLine().trim()
-    }
-
-    override fun pedirDouble(msj: String): Double? {
-        return pedirInfo(msj).replace(',', '.').toDoubleOrNull()
-    }
-
-    override fun pedirEntero(msj: String): Int? {
-        return pedirInfo(msj).toIntOrNull()
-    }
-
-    override fun preguntar(msj: String): Boolean {
-        do {
-            val respuesta = pedirInfo(msj).lowercase()
-            when (respuesta) {
-                "s", "si" -> return true
-                "n", "no" -> return false
-                else -> mostrarError("Respuesta no válida. Responde con s, n, si o no.")
-            }
-        } while (true)
-    }
-
-    override fun limpiarPantalla(numSaltos: Int) {
-        if (System.console() != null) {
-            mostrar("\u001b[H\u001b[2J", false)
-            System.out.flush()
-        } else {
-            repeat(numSaltos) {
-                mostrar("")
-            }
+        if (msj.isNotEmpty()){
+            mostrar(msj)
         }
+        return scanner.nextLine()
     }
 
 }
-
